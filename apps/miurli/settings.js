9717e521ad357cca9d82853deb8102d68df7de8c
longfinger65 <43702171+longfinger65@users.noreply.github.com>
2020-06-20 21:08:30 +0200

    Update settings.js

--- apps/miurli/settings.js
+++ apps/miurli/settings.js
@@ -1,16 +1,35 @@
 // make sure to enclose the function in parentheses
 (function(back) {
-  let settings = require('Storage').readJSON('miurli.json',1)||{};
-  function save(key, value) {
-    settings[key] = value;
-    require('Storage').write('miurli.json',settings);
+  
+  const SETTINGS_FILE = 'miurli.settings.json';
+  //const LINES = ['Steps', 'Distance'];
+
+  // initialize with default settings...
+  let s = {
+    'hourBeep' : 'yes',
+  };
+  // ...and overwrite them with any saved values
+  // This way saved values are preserved if a new version adds more settings
+  const storage = require('Storage');
+  const saved = storage.readJSON(SETTINGS_FILE, 1) || {};
+  for (const key in saved) {
+    s[key] = saved[key];
+  }
+  // creates a function to safe a specific setting, e.g.  save('color')(1)
+  function save(key) {
+    return function (value) {
+      s[key] = value;
+      storage.write(SETTINGS_FILE, s);
+      //WIDGETS["activepedom"].draw();
+    };
   }
   const appMenu = {
-    '': {'title': 'App Settings'},
+    '': {'title': 'Mi Urli Settings'},
     '< Back': back,
     'Hour-Beep': {
-      value: settings.hourBeep||12,
-      onchange: (m) => {save('hourBeep', m)}
+      value: s.hourBeep,
+      format: v => v?"Yes":"No",
+      onchange: save('hourBeep'),}
     }   
   };
   E.showMenu(appMenu)
